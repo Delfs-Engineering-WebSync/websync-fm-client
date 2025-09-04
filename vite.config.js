1c4a4c2ad2634ac12a45d7e9b0c283a4cb8efb2f
Charles Delfs <charlesdelfs@iMac-M3.local>
2025-09-04 10:28:11 -0400

    Dev server fix: remove vite-plugin-vue-devtools from config; cleanup temp configs; ensure npm run dev starts reliably; ignore PID files

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), viteSingleFile(), tailwindcss()],
+  plugins: [vue(), viteSingleFile(), tailwindcss()],
   build: {
     // Use terser so we can control comment removal in JS
     minify: 'terser',
